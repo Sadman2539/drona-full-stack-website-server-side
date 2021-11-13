5a7d8adcf8b1bcd0c7ca25c71f38205e7c6b8fe3
Sadman2539 <cadetsadman39@gmail.com>
2021-11-13 15:03:31 +0800

    code optimized

--- index.js
+++ index.js
@@ -21,12 +21,79 @@ async function run() {
         await client.connect();
         const database = client.db("drona");
         const orderCollection = database.collection("orders");
-        app.post('/orders', async (req, res) => {
+        const productCollection = database.collection("products");
+        const reviewCollection = database.collection("reviews");
+        const blogCollection = database.collection("blogs");
+
+
+        //GET API (Products)
+        app.get('/explore', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.limit(10).toArray();
+            res.send(products);
+
+        });
+        // post all Products
+        app.post('/explore', async (req, res) => {
             const product = req.body;
             const result = await orderCollection.insertOne(product);
             console.log(result);
             res.json(result);
         });
+        //GET API (Reviews)
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+
+        });
+        // post all Reviews
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        });
+        // GET API(orders)
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.limit(10).toArray();
+            res.send(orders);
+
+        });
+
+        // post all orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            console.log(result);
+            res.json(result);
+        });
+        // GET API(blogs)
+        app.get('/blogs', async (req, res) => {
+            const cursor = blogCollection.find({});
+            const blogs = await cursor.limit(10).toArray();
+            res.send(blogs);
+
+        });
+
+        // post all blogs
+        app.post('/blogs', async (req, res) => {
+            const blog = req.body;
+            const result = await blogCollection.insertOne(blog);
+            console.log(result);
+            res.json(result);
+        });
+
+
+        //GET Single Product 
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific id');
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.json(product);
+        })
     }
     finally {
         // await client.close();
